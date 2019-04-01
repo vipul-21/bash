52b9bba638e81f2da867b7959c3f78bb2ef4f25f
vipul <vipul21sept@gmail.com>
2019-03-31 19:33:15 -0700

    added data via variables

--- variables.sh
+++ variables.sh
@@ -1,6 +1,13 @@
 #!#/bin/bash
 
-firstvar=Hello
-secondvar=world
+secondvar="world"
+firstvar="Hello $secondvar"
 
-echo $firstvar $secondvar
+echo $firstvar
+
+thirdvar=$( ls /etc | wc -l )
+echo there are $thirdvar entries in directory /etc
+
+echo Number of arguments $#, the values were $@
+echo The user : $USER
+echo The time spent : $SECONDS
